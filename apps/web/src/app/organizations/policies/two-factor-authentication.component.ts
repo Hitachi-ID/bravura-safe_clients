e9cb48f532a1d8cc1bf927ddc27c7436f4922db3
gisselac <gisselac@hitachi-id.com>
2022-07-10 03:53:02 -0400

    Fixes after upgrade to 2022.6.0.

--- apps/web/src/app/organizations/policies/two-factor-authentication.component.ts
+++ apps/web/src/app/organizations/policies/two-factor-authentication.component.ts
@@ -1,7 +1,7 @@
 import { Component } from "@angular/core";
 
-import { PolicyType } from "jslib-common/enums/policyType";
-import { Organization } from "jslib-common/models/domain/organization";
+import { PolicyType } from "@bitwarden/common/enums/policyType";
+import { Organization } from "@bitwarden/common/models/domain/organization";
 
 import { BasePolicy, BasePolicyComponent } from "./base-policy.component";
 
