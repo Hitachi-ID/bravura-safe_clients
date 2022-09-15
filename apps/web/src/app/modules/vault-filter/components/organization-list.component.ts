e736607fc1cc2ba94245578b2ccde19c2d3423b7
gisselac <gisselac@hitachi-id.com>
2022-09-15 15:08:08 -0400

    Fixed issue when selecting an organization in organizations dropdown.

--- apps/web/src/app/modules/vault-filter/components/organization-list.component.ts
+++ apps/web/src/app/modules/vault-filter/components/organization-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { Router } from "@angular/router";
 
 import { OrganizationFilterComponent as BaseOrganizationFilterComponent } from "@bitwarden/angular/modules/vault-filter/components/organization-filter.component";
 
@@ -8,4 +9,12 @@ import { OrganizationFilterComponent as BaseOrganizationFilterComponent } from "
 })
 export class OrganizationListComponent extends BaseOrganizationFilterComponent {
   displayText = "allVaults";
+
+  constructor(
+    private router: Router
+  ) { super() }
+
+  navigateOrg(id: string): void {
+    this.router.navigate(["/organizations", id]);
+  }
 }
