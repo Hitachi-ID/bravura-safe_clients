4e651e56337f803e19613b092a77e3e8f8c47f3a
Tony M <tonym@hitachi-id.com>
2023-05-08 10:49:39 -0400

    BSAF-33 update signing script to use signtool

--- apps/desktop/sign.js
+++ apps/desktop/sign.js
@@ -4,16 +4,14 @@ exports.default = async function (configuration) {
   if (parseInt(process.env.ELECTRON_BUILDER_SIGN) === 1 && configuration.path.slice(-4) == ".exe") {
     console.log(`[*] Signing file: ${configuration.path}`);
     require("child_process").execSync(
-      `azuresigntool sign -v ` +
-        `-kvu ${process.env.SIGNING_VAULT_URL} ` +
-        `-kvi ${process.env.SIGNING_CLIENT_ID} ` +
-        `-kvt ${process.env.SIGNING_TENANT_ID} ` +
-        `-kvs ${process.env.SIGNING_CLIENT_SECRET} ` +
-        `-kvc ${process.env.SIGNING_CERT_NAME} ` +
-        `-fd ${configuration.hash} ` +
+      `signtool sign /q ` +
+        `/f ${process.env.CERT_KEYPATH} ` +
+        `/p ${process.env.CERT_KEYPASS} ` +
+        `/fd ${configuration.hash} ` +
+        `/td ${configuration.hash} ` +
         `-du ${configuration.site} ` +
         `-tr http://timestamp.digicert.com ` +
-        `${configuration.path}`,
+        `"${configuration.path}"`,
       {
         stdio: "inherit",
       }
