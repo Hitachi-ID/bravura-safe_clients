69510f09ccd7b382124880a1c7a420b60915bfe5
Admin <noreply@hitachi-id.com>
2023-06-30 10:40:35 -0400

    BSAF-108 correct app store signing and build number

--- apps/desktop/scripts/after-sign.js
+++ apps/desktop/scripts/after-sign.js
@@ -17,9 +17,16 @@ async function run(context) {
   const macBuild = context.electronPlatformName === "darwin";
   const copyPlugIn = ["darwin", "mas"].includes(context.electronPlatformName);
 
+  console.log("### Copy safari plugin value: " + copyPlugIn);
+
   if (copyPlugIn) {
     // Copy Safari plugin to work-around https://github.com/electron-userland/electron-builder/issues/5552
-    const plugIn = path.join(__dirname, "../PlugIns");
+    let plugIn = path.join(__dirname, "../PlugIns");
+    if (context.electronPlatformName === "mas") {
+      plugIn = path.join(__dirname, "../PlugIns_mas");
+    }
+
+    console.log("### Copy safari plugin " + plugIn);
     if (fse.existsSync(plugIn)) {
       fse.mkdirSync(path.join(appPath, "Contents/PlugIns"));
       fse.copySync(
@@ -27,8 +34,11 @@ async function run(context) {
         path.join(appPath, "Contents/PlugIns/safari.appex")
       );
 
+      console.log("### Finished: Copy safari plugin " + plugIn);
+
       // Resign to sign safari extension
       if (context.electronPlatformName === "mas") {
+        console.log("// Resign to sign safari extension")
         const masBuildOptions = deepAssign(
           {},
           context.packager.platformSpecificBuildOptions,
@@ -48,15 +58,24 @@ async function run(context) {
     }
   }
 
-  if (macBuild) {
+  const appleId = process.env.APPLE_ID_USERNAME || process.env.APPLEID;
+  if (macBuild && appleId) {
     console.log("### Notarizing " + appPath);
-    const appleId = process.env.APPLE_ID_USERNAME || process.env.APPLEID;
+    // const appleId = process.env.APPLE_ID_USERNAME || process.env.APPLEID;
     const appleIdPassword = process.env.APPLE_ID_PASSWORD || `@keychain:AC_PASSWORD`;
+
+    console.log("### Notarizing *** *** apple ID *** " + appleId);
+    console.log("### Notarizing *** remember to use app specific password for this apple ID **** ");
     return await notarize({
       appBundleId: "com.hitachiid.desktop",
       appPath: appPath,
       appleId: appleId,
       appleIdPassword: appleIdPassword,
     });
+  }
+  else {
+    console.log("__**__");
+    console.log("__**__ Notarizing skipped ***  not a macbuild or env vars ( APPLE_ID_USERNAME or APPLEID ) not set *** *** ");
+    console.log("__**__");
   }
 }
